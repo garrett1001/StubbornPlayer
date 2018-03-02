4663b43c3a4bdeec5e2522ea24173b06e1f87f8d
garrett1001 <gwelton@mail.csuchico.edu>
2018-03-01 23:46:53 -0800

    complete

--- main.cpp
+++ main.cpp
@@ -6,6 +6,25 @@ using namespace std;
 int main()
 {
   SecretDoor game;
+  int repeat;
+  int won = 0;
+
+  cout<<"how many times do you want to repeat the game?"<<endl;
+  cin>>repeat;
+
+  for(int i=0; i<repeat; i++)
+  {
+   game.newGame();
+   game.guessDoorC();
+   game.guessDoorC();
+
+   if(game.isWinner() == true)
+   {
+    won++;
+   }
+  }
+
+  cout<<"you have won "<<won<<" times"<<endl;
     
   return 0;
 }
